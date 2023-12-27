95d069cb31f3c396559477f36ea7fc39afc5053a
SigningOwl <whddms1208@gmail.com>
2023-12-27 18:28:45 +0900

    Change region and clustername in variable.tf and s3 bucket name, region in terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions-owl"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-northeast-2"
   }
 
   required_version = "~> 1.6.3"
